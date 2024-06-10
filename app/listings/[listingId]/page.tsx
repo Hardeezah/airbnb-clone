03f686aee643d98bf13e8f6551c02d8c091468d0
Hardeezah <mohammedhardeeza@gmail.com>
2024-06-10 14:04:39 +0100

    listing

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -3,6 +3,7 @@ import getListingById from "@/app/actions/getListingById";
 import EmptyState from "@/app/components/EmptyState";
 import ListingClient from "./ListingClient";
 import getReservations from "@/app/actions/getReservations";
+import { SafeUser } from "@/app/types";
 
 interface IParams{
     listingId?: string;
@@ -11,7 +12,7 @@ interface IParams{
 const ListingPage = async ({params}: {params: IParams}) => {
     const listing = await getListingById(params);
     const reservations = await getReservations(params);
-    const currentUser = await getCurrentUser();
+    const currentUser = await getCurrentUser() as SafeUser | null;
 
     if(!listing){
         return (
